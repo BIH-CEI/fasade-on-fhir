969bb855a4f42911ab8e53ce9df15596e10167b6
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-08-01 11:12:53 +0200

    prepre records from meta data

---
+++ src/erkeronfhir/convert/metadata.py
@@ -0,0 +1,39 @@
+from typing import Dict, List
+
+FIELD_CHOICES = "select_choices_or_calculations"
+FIELD_NAME = "field_name"
+
+
+def fill_metadata(records: List[Dict[str, str]], metadata: List[Dict]):
+    choices = _get_choices(metadata)
+    _replace_with_choices(records, choices)
+
+
+def _get_choices(metadata: List[Dict[str, str]]) -> Dict[str, Dict[str, str]]:
+    result = {}
+    for entry in metadata:
+        choices = entry.get(FIELD_CHOICES, None)
+        if choices:
+            choices = choices.split("|")
+
+            if len(choices) < 2:
+                continue
+
+            options = {}
+            for choice in choices:
+                key, value = tuple(choice.split(",", maxsplit=1))
+                options[key.strip()] = value.strip()
+            result[entry[FIELD_NAME]] = options
+
+    return result
+
+
+def _replace_with_choices(
+    records: List[Dict[str, str]], choices: Dict[str, Dict[str, str]]
+) -> None:
+    for entry in records:
+        for key in entry.keys():
+            if key in choices:
+                value = choices[key].get(entry[key], None)
+                if value:
+                    entry[key] = value
