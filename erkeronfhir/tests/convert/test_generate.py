5ca67941c8078f3bb608ea71d56f8d54ad777375
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-08-09 10:10:22 +0200

    adjust config and setup to let tests run

--- erkeronfhir/tests/convert/test_generate.py
+++ erkeronfhir/tests/convert/test_generate.py
@@ -1,18 +1,19 @@
 import unittest
 
+from erkeronfhir.config import config
 from erkeronfhir.convert.generate import create_from_single
 from fhir.resources.patient import Patient
 
 
 class TestGenerateFromSingle(unittest.TestCase):
     def test_simple(self):
-        mappings = {"Patient": {"name": "PatientName"}}
+        config.mapping = {"Patient": {"name": "PatientName"}}
 
         PATIENT_NAME = "TestPatient"
 
-        record = {"PatientName": PATIENT_NAME}
+        record = {"PatientName": PATIENT_NAME, "record_id": 1}
 
-        result = create_from_single("Patient", record, mappings)
+        result = create_from_single("Patient", record)
 
         self.assertIsInstance(result, Patient)
         self.assertEqual(result.name, PATIENT_NAME)
