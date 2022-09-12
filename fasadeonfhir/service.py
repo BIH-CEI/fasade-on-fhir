9709275e6c8e558bb0ae4594910c6398c21ea425
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-12 11:44:37 +0200

    remove specific implemenation from `service`

--- fasadeonfhir/service.py
+++ fasadeonfhir/service.py
@@ -1,18 +1,11 @@
 from typing import List
 
-import fhir.resources
-from fhir.resources.capabilitystatement import CapabilityStatement
-
 from fasadeonfhir.config import config
 from fasadeonfhir.convert.generate import Mapper
-from fasadeonfhir.redcap.provider import RedcapProvider
-
-__app_name__ = "REDCap on FHIR"
 
 
 class Service:
     def __init__(self) -> None:
-        self.provider = RedcapProvider(config.redcap.api_url, config.redcap.api_token)
         self.mapper = Mapper(
             config.mapping.resources,
             config.mapping.substitutions,
@@ -29,23 +22,3 @@ class Service:
     def _get_resources(self, resource_filter: List[str] = None):
         records = self.provider.get_records()
         return self.mapper.create_from_list(records, resource_filter=resource_filter)
-
-    def get_capability(self):
-        definition = {
-            "software": {"name": __app_name__},
-            "fhirVersion": fhir.resources.__fhir_version__,
-            "status": "active",
-            "kind": "capability",
-            "date": "2022-08-30",
-            "format": ["json"],
-            "rest": [
-                {
-                    "mode": "server",
-                    "resource": [
-                        {"type": "Patient", "interaction": [{"code": "read"}]},
-                        {"type": "Observation", "interaction": [{"code": "read"}]},
-                    ],
-                },
-            ],
-        }
-        return CapabilityStatement(**definition)
