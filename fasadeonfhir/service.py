f404f7e7d70df722370f30d8fd94c14618346c65
Alexander Schulze <alexander.schulze@bih-charite.de>
2022-09-12 13:39:32 +0200

    add automatic compability statement generation

--- fasadeonfhir/service.py
+++ fasadeonfhir/service.py
@@ -1,10 +1,17 @@
 from typing import List
 
+from fhir.resources import __fhir_version__
+from fhir.resources.capabilitystatement import CapabilityStatement
+
 from fasadeonfhir.config import config
 from fasadeonfhir.convert.generate import Mapper
 
 
 class Service:
+    __app_name__ = "Fasade on FHIR"
+    __capabilities_rest__ = {}
+    __capabilities_date__ = ""
+
     def __init__(self) -> None:
         self.mapper = Mapper(
             config.mapping.resources,
@@ -22,3 +29,28 @@ class Service:
     def _get_resources(self, resource_filter: List[str] = None):
         records = self.provider.get_records()
         return self.mapper.create_from_list(records, resource_filter=resource_filter)
+
+    def get_capability(self):
+        definition = {
+            "software": {"name": self.__app_name__},
+            "fhirVersion": __fhir_version__,
+            "status": "active",
+            "kind": "capability",
+            "date": self.__capabilities_date__,
+            "format": ["json"],
+            "rest": [
+                {
+                    "mode": "server",
+                },
+            ],
+        }
+
+        if self.__capabilities_rest__:
+            definition["rest"][0]["resource"] = []
+            resources = definition["rest"][0]["resource"]
+
+            for resource, capability in self.__capabilities_rest__.items():
+                interactions = [{"code": item} for item in capability]
+                resources.append({"type": resource, "interaction": interactions})
+
+        return CapabilityStatement(**definition)
